63355ffb4f56027d6dd5e3e69e7484f637da34a7
ibeingakash <75716531+ibeingakash@users.noreply.github.com>
2021-07-03 16:28:50 +0530

    Create bts.py

---
+++ bts.py
@@ -0,0 +1,30 @@
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        queue = self.deque([root]) if root else self.deque()
+
+        while queue:
+            node = queue.popleft()
+            print(node.data, end=' ')
+
+            if node.left: queue.append(node.left)
+            if node.right: queue.append(node.right)
+
+T=int(input())
